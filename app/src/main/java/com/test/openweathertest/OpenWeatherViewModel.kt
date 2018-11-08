a13b206ec76cb4b1eb16b4e2d9179bcb686b3542
Sumeet Verma <sverma@andigital.com>
2018-11-08 00:10:52 +0000

    started on viewmodel

--- app/src/main/java/com/test/openweathertest/OpenWeatherViewModel.kt
+++ app/src/main/java/com/test/openweathertest/OpenWeatherViewModel.kt
@@ -1,8 +1,35 @@
 package com.test.openweathertest
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.test.domain.openweather.OpenWeatherRepositoryRx
+import com.test.domain.openweather.networkmodel.Response
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class OpenWeatherViewModel(val openWeatherRepositoryRx: OpenWeatherRepositoryRx) : ViewModel() {
+    val result: MutableLiveData<Response> = MutableLiveData()
+
+    val error: MutableLiveData<Throwable> = MutableLiveData()
+
+    val compositeDisposable : CompositeDisposable = CompositeDisposable()
+
+    fun getWeatherForTheDay(city : String) {
+        openWeatherRepositoryRx.getWeatherByDay(city)
+                .subscribeOn(Schedulers.io())
+                ?.observeOn(AndroidSchedulers.mainThread())
+                ?.subscribe({handleResponse(it)}, {handleError(it)})
+                ?.let { compositeDisposable.add(it) }
+    }
+
+    fun handleResponse(response: Response) {
+        result.value = (response)
+    }
+
+    fun handleError(throwable: Throwable) {
+        error.value = throwable
+    }
+
 
 }
